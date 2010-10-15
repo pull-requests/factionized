cc1dc490f1c65235f0dd7ff278100c33183ecc3b
Harley <harley@bigdoor.com>
2010-10-15 15:24:36 -0700

    Initial models sketch

---
+++ models.py
@@ -0,0 +1,41 @@
+from google.appengine.ext import db
+
+class Game(db.Model):
+
+    name = db.StringProperty(required=True)
+
+
+class Profile(db.Model):
+    
+    name = db.StringProperty(required=True)
+
+
+class Role(db.Model):
+
+    type_id = db.IntegerProperty(required=True)
+    game = db.ReferenceProperty(Game, required=True)
+    profile = db.ReferenceProperty(Profile, 
+                                   required=True, 
+                                   collection_set='roles')
+
+
+class Round(db.Model):
+
+    number = db.IntegerProperty(required=True)
+    started = db.DateTimeProperty(auto_now_add=True, 
+                                  required=True)
+
+
+class Activity(db.Model):
+
+    type_id = db.IntegerProperty(required=True)
+    actor = db.ReferenceProperty(Role,
+                                 required=True, 
+                                 collection_set='initiated_actions')
+    target = db.ReferenceProperty(Role, 
+                                  required=True, 
+                                  collection_set='received_actions')
+    occurred = db.DateTimeProperty(auto_now_add=True, 
+                                   required=True)
+    message = db.TextProperty()
+
