eebcfcb76198bb896a716743ab79ef45c3df72ed
Lee McFadden <lee@bigdoor.com>
2010-10-16 15:06:05 -0700

    Added a Slot model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from google.appengine.ext import db
+from app.lib.uid import new_uid
 
 class UIDModel(db.Model):
+    """Base class to give models a nicer, URL friendly ID.
+    """
     def __init__(self, *args, **kw):
         if not 'key_name' in kw and not '_from_entity' in kw:
             kw['key_name'] = new_uid()
@@ -16,33 +19,36 @@ class UIDModel(db.Model):
     def get_by_uid(cls, uid):
         return cls.get_by_key_name(uid)
 
-class Game(db.Model):
+class Game(UIDModel):
+    name = db.StringProperty(required=True)
+    game_starter = db.KeyProperty(required=True)
 
+class Profile(UIDModel):
     name = db.StringProperty(required=True)
 
-
-class Profile(db.Model):
-    
-    name = db.StringProperty(required=True)
-
-
-class Role(db.Model):
+class Role(UIDModel):
 
     type_id = db.IntegerProperty(required=True)
     game = db.ReferenceProperty(Game, required=True)
-    profile = db.ReferenceProperty(Profile, 
-                                   required=True, 
+    profile = db.ReferenceProperty(Profile,
+                                   required=True,
                                    collection_name='roles')
 
 
-class Round(db.Model):
+class Slot(UIDModel):
+    player = db.ReferenceProperty(Profile)
+    game = db.ReferenceProperty(Game)
+    role = db.ReferenceProperty(Role)
+    in_play = db.BooleanProperty(default=True)
+
+class Round(UIDModel):
 
     number = db.IntegerProperty(required=True)
-    started = db.DateTimeProperty(auto_now_add=True, 
+    started = db.DateTimeProperty(auto_now_add=rue,
                                   required=True)
 
 
-class Activity(db.Model):
+class Activity(UIDModel):
 
     type_id = db.IntegerProperty(required=True)
     actor = db.ReferenceProperty(Role,
