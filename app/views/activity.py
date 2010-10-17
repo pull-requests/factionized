a254db93014521b81440b3aca5f67fc9d85eab9f
Lee McFadden <lee@bigdoor.com>
2010-10-17 14:28:31 -0700

    Fixed some typos

--- app/views/activity.py
+++ app/views/activity.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from app.decorators import login_required
-from app.models import Game, Round, Thread, Activity, Message, Vote
+from app.models import Game, Thread, Activity, Message, Vote
 from app.shortcuts import json
 
 def get_activities(cls, user, thread_id):
-    thread = Thread.get_by_id(thread_id))
+    thread = Thread.get_by_id(thread_id)
     if not thread:
         return HttpResponse('Not Found', status=404)
     if not thread.user_can_view(user):
@@ -13,16 +13,15 @@ def get_activities(cls, user, thread_id):
                  cls.all().
                      filter('thread =', thread).
                      order('created')])
-    
 
 @login_required
 def activities(request, game_id, round_id, thread_id):
-    return get_activities(Activity, reqeust.user, thread_id)
+    return get_activities(Activity, request.user, thread_id)
 
 @login_required
 def votes(request, game_id, round_id, thread_id):
-    if reqeust.method == 'GET':
-        return get_activities(Vote, reqeust.user, thread_id)
+    if request.method == 'GET':
+        return get_activities(Vote, request.user, thread_id)
 
 @login_required
 def messages(request, game_id, round_id, thread_id):
